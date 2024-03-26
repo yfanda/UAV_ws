a6ad07689f89b958b6350e79d7ce679773effb73
Yifan.Zhang <yifan.zhang0215@gmail.com>
2024-03-26 16:28:31 +0100

    communicate with px4 msgs

--- src/mpc_node/src/mpc_node.cpp
+++ src/mpc_node/src/mpc_node.cpp
@@ -1,6 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "common_msgs/msg/state.hpp" 
 #include "common_msgs/msg/input.hpp" 
+#include <px4_msgs/msg/sensor_combined.hpp>
+#include <px4_msgs/msg/RaspberryPiToPixhawk.hpp>
+#include <px4_msgs/msg/PixhawkToRaspberryPi.hpp>
 #include "libs/grampc.hpp"
 #include "grampcd/interface/dmpc_interface.hpp"
 #include <memory>
@@ -96,6 +99,24 @@ public:
 
         subscriber_ = this->create_subscription<common_msgs::msg::State>(
             "new_state", 10, std::bind(&ControlNode::compute_and_publish_input, this, std::placeholders::_1));
+        subscription_ = this->create_subscription<px4_msgs::msg::SensorCombined>(
+			"SensorCombined_PubSubTopic",
+			10,
+			[this](const px4_msgs::msg::SensorCombined::UniquePtr msg) {
+			std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+			std::cout << "RECEIVED SENSOR COMBINED DATA"   << std::endl;
+			std::cout << "============================="   << std::endl;
+			std::cout << "ts: "          << msg->timestamp    << std::endl;
+			std::cout << "gyro_rad[0]: " << msg->gyro_rad[0]  << std::endl;
+			std::cout << "gyro_rad[1]: " << msg->gyro_rad[1]  << std::endl;
+			std::cout << "gyro_rad[2]: " << msg->gyro_rad[2]  << std::endl;
+			std::cout << "gyro_integral_dt: " << msg->gyro_integral_dt << std::endl;
+			std::cout << "accelerometer_timestamp_relative: " << msg->accelerometer_timestamp_relative << std::endl;
+			std::cout << "accelerometer_m_s2[0]: " << msg->accelerometer_m_s2[0] << std::endl;
+			std::cout << "accelerometer_m_s2[1]: " << msg->accelerometer_m_s2[1] << std::endl;
+			std::cout << "accelerometer_m_s2[2]: " << msg->accelerometer_m_s2[2] << std::endl;
+			std::cout << "accelerometer_integral_dt: " << msg->accelerometer_integral_dt << std::endl;
+		});
     }
 
 private:
