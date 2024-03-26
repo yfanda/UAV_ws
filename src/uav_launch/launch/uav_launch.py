cfe52a7ab1f676933634de139f4bac20ac2e71ae
Yifan.Zhang <yifan.zhang0215@gmail.com>
2024-03-26 14:29:48 +0100

    modified launch.py

--- src/uav_launch/launch/uav_launch.py
+++ src/uav_launch/launch/uav_launch.py
@@ -5,19 +5,20 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='simulator_node', 
-            executable='simulator_node_exe', 
-            name='simulator_node',
+            package='mpc_node', 
+            executable='mpc_node_exe', 
+            name='mpc_node',
             output='screen'
+            
         ),
         TimerAction(
             period=5.0,  # 5秒延时
             actions=[
                 Node(
-                    package='mpc_node', 
-                    executable='mpc_node_exe', 
-                    name='mpc_node',
-                    output='screen'
+                    package='simulator_node', 
+		    executable='simulator_node_exe', 
+		    name='simulator_node',
+		    output='screen'
                 )
             ]
         )
